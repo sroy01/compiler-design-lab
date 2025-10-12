16efc0f9fd14c47f866d4e99a100d97885c0e537
SR Dashboard 2 <supanroyzoom@gmail.com>
2025-10-12 13:43:37 +0600

    Create day2_4.c

---
+++ day2_4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char s[1000];
+    fgets(s, sizeof(s), stdin);
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == ' ') {
+            printf("\n"); 
+        } else if (s[i] != '\n') {
+            printf("%c", s[i]);
+        }
+    }
+    return 0;
+}

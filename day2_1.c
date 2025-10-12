86e41137eccc0858f0cd8063db1446b6d2b3a463
SR Dashboard 2 <supanroyzoom@gmail.com>
2025-10-12 12:07:48 +0600

    Create day2_1.c

---
+++ day2_1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int main(){
+    char s[1000];
+    gets(s);
+    int v=0, cn=0, d=0;
+    for(int i=0;s[i]!='\0';i++){
+        char c = tolower(s[i]);
+        if(isdigit(c)){
+            d++;
+        } else if(c>='a' && c<='z'){
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                v++;
+            }else {
+                cn++;
+            }
+        }
+    }
+        printf("Total Vowels: %d\n", v);
+        printf("Total Consonants: %d\n", cn);
+        printf("Total Digits: %d", d);
+        return 0;
+}

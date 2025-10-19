a5a557c2925e3896d2b6d04999cf2316667aa384
SR Dashboard 2 <supanroyzoom@gmail.com>
2025-10-19 12:27:32 +0600

    Create nextthree.c

---
+++ day3/nextthree.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char s[1000];
+    scanf("%s",s);
+
+    for(int i=0;i<strlen(s);i++){
+        if(s[i]>='a' && s[i]<='z'){
+            s[i]=(s[i]+3);
+        } else if(s[i]>='A' && s[i]<='Z'){
+            s[i]=s[i]+3;
+        }
+    }
+    printf("%s", s);
+    return 0;
+}
